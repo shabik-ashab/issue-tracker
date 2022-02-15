197d822226a6660323170bd13167f3a9943cde50
nub69 <shabik.ashab@gmail.com>
2022-02-15 10:51:08 +0600

    currentuser undefined fix

--- src/Components/Dashboard/AssignTicket.jsx
+++ src/Components/Dashboard/AssignTicket.jsx
@@ -1,14 +1,20 @@
-import { Button, FormControl, Grid, InputLabel, MenuItem, Select, Typography } from '@mui/material';
+import { Button, FormControl, Grid, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import React, { useEffect, useState } from 'react';
 import useAuth from '../../hooks/useAuth';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
 
 const AssignTicket = () => {
     const {currentUser} = useAuth();
 
     const [tickets, setTickets] = useState([]);
     const [users,setUsers] = useState([]);
-
+  
+    // const [deadline,setDeadline] = React.useState({
+    //   deadline:new Date(),
+    // });
     const [loginData, setLoginData] = React.useState({
        
       });
@@ -57,7 +63,16 @@ const AssignTicket = () => {
         newLoginData[field] = value;
         setLoginData(newLoginData);
       };
-//   console.log(loginData);
+      // const handleDateChange = (e) => {
+
+      //   // const id = e.target.id;
+      //   const field = e.target.name;
+      //   const value = e.target.value;
+      //   const newLoginData = { ...loginData };
+      //   newLoginData[field] = value;
+      //   setDeadline(newLoginData);
+      // };
+  // console.log(loginData);
 //   console.log(tickets);
 
   const handleConfirm = (id) => {
@@ -80,15 +95,18 @@ const AssignTicket = () => {
     })
     .finally();
 }
-     
+
+// const [value, setValue] = React.useState(new Date());
+//    console.log(value);  
   return <div>
 
 {tickets.map((ticket) => (
             <Box
+             key={ticket._id}
               sx={{ mb: 1, width: "100%", border: "1px solid #546e7a", p: 1 }}
             >
               <Grid container spacing={2}>
-                <Grid item xs={7}>
+                <Grid item xs={3}>
                  <Box display="flex" sx={{alignContent: 'center'}}>
                  <Typography  variant="h6">{ticket.title} </Typography>
                   {
@@ -154,32 +172,78 @@ const AssignTicket = () => {
                  </Typography>
                   <Typography>{ticket.details}</Typography>
                 </Grid>
-                <Grid item xs={3}>
-                <Box sx={{p:1}}>
-                <form >
-                <FormControl display="flex"  fullWidth>
-           
-                <InputLabel >Assign To</InputLabel>
-                 <Select
-                 
-                   value={loginData.assign}
-                   label="Assign to"
-                   onChange={handleChange}
-                   name="assign"
-                   id={ticket._id}
-                  //  onBlur={handleOnBlur}
-                 >
-                   {
-                       users.map((user) => (
-                        <MenuItem value={user.displayName}>{user.displayName}</MenuItem>
-                       ))
-                   }
-                 </Select>
+                <Grid tem xs={3}>
+               {/* <Box sx={{p:2}}>
+               <LocalizationProvider dateAdapter={AdapterDateFns}>
+ <form>
+ <DatePicker
+    label="Basic example"
+    value={value.deadline}
+    id={ticket._id}
+    name="deadline"
+    onChange={(newValue) => {
+      let obj = {...value}
+      obj = {
+        deadline:newValue,
+        id:ticket._id
+      }
+      obj[i].selected = !this.state.obj[i].selected
+      setValue({obj});
+    }}
+    
+    renderInput={(params) => <TextField {...params} />}
+  />
+ </form>
+</LocalizationProvider>
+               </Box> */}
+
+               
+                </Grid>
+                <Grid item xs={4}>
+                {
+               ticket.assign ?
+               <Typography>
+                Already assigned To: { ticket.assign}
+             </Typography>
+             :
+             <Box sx={{p:1}}>
+
+             <Grid container spacing={2}>
             
-                 <Button onClick={() => handleConfirm(ticket._id)} variant="outlined">confirm</Button>
-               </FormControl>
-                </form>
-                </Box>
+<Grid item xs={6}>
+
+<FormControl  fullWidth>
+         
+         <InputLabel >Assign To</InputLabel>
+          <Select
+          
+            value={loginData.assign}
+            label="Assign to"
+            onChange={handleChange}
+            name="assign"
+            id={ticket._id}
+           //  onBlur={handleOnBlur}
+          >
+            {
+                users.map((user) => (
+                 <MenuItem value={user.displayName}>{user.displayName}</MenuItem>
+                ))
+            }
+          </Select>
+     
+          
+        </FormControl>
+</Grid>
+<Grid item xs={6}>
+<Button sx={{mt:1}} onClick={() => handleConfirm(ticket._id)} variant="outlined">confirm</Button>
+</Grid>
+
+</Grid>
+
+            
+              </Box>
+           }
+               
                 </Grid>
                 <Grid item xs={2}>
                 <Typography sx={{fontSize:'.6em'}}>
--- src/Components/Dashboard/ResponsiveDrawer.jsx
+++ src/Components/Dashboard/ResponsiveDrawer.jsx
@@ -10,19 +10,32 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import useAuth from "../../hooks/useAuth";
-import { Button, Grid } from "@mui/material";
-import { Switch, Route, Link, useRouteMatch,useLocation, useHistory, } from "react-router-dom";
+import { Button, CircularProgress, Grid } from "@mui/material";
+import {
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useLocation,
+  useHistory,
+} from "react-router-dom";
 import Team from "./Team";
-import Ticket from './Ticket';
+import Ticket from "./Ticket";
 import AssignTicket from "./AssignTicket";
-import ManagerRoute from '../ManagerRoute/ManagerRoute';
+import ManagerRoute from "../ManagerRoute/ManagerRoute";
+import MyTicket from "./MyTicket";
 
 const drawerWidth = 240;
 
 function ResponsiveDrawer(props) {
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
-  const { currentUser, user,logOut,manager } = useAuth();
+  const [loading, setLoading] = React.useState(true);
+  const { currentUser, logOut } = useAuth();
+
+  // if (currentUser.email) {
+  //   setLoading(false);
+  // }
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
@@ -33,66 +46,87 @@ function ResponsiveDrawer(props) {
 
   const drawer = (
     <div>
-      <Box sx={{ ml: 2, mb:1 }}>
-        <Typography>Name: {currentUser?.displayName}
-        <Button onClick={()=>logOut(history)} >
-                  logout
-                 </Button>
+      <Box sx={{ ml: 2, mb: 1 }}>
+        <Typography>
+          Name: {currentUser?.displayName}
+          <Button onClick={() => logOut(history)}>logout</Button>
         </Typography>
         <Typography>Team Name: {currentUser?.team}</Typography>
       </Box>
-      
-      <Toolbar />
-      
-      <Link to={`${url}`} style={{ textDecoration: 'none', color:'#1769aa' }}>
-     
-      <Box sx={{
-        backgroundColor: '',
-        '&:hover': {
-          backgroundColor: '#e0e0e0',
-          opacity: [0.9, 0.8, 0.7],
-        },
-        p:1,
-        m:1
-      }}>
-         <span style={{marginLeft: '4em'}}> My Team</span>
-      </Box>
-     
-      </Link>
-      <Link to={`${url}/ticket`} style={{ textDecoration: 'none', color:'#1769aa' }}>
-     
-      <Box sx={{
-        backgroundColor: '',
-        '&:hover': {
-          backgroundColor: '#e0e0e0',
-          opacity: [0.9, 0.8, 0.7],
-        },
-        p:1,
-        m:1
-      }}>
-         <span style={{marginLeft: '4em'}}> Ticket </span>
-      </Box>
-     
-      </Link>
-     {
-       currentUser?.role == 'manager' &&
-       <Link to={`${url}/assign`} style={{ textDecoration: 'none', color:'#1769aa' }}>
-     
-       <Box sx={{
-         backgroundColor: '',
-         '&:hover': {
-           backgroundColor: '#e0e0e0',
-           opacity: [0.9, 0.8, 0.7],
-         },
-         p:1,
-         m:1
-       }}>
-          <span style={{marginLeft: '4em'}}> Assign Ticket </span>
-       </Box>
-      
-       </Link>
-     }
 
+      <Toolbar />
+
+      <Link to={`${url}`} style={{ textDecoration: "none", color: "#1769aa" }}>
+        <Box
+          sx={{
+            backgroundColor: "",
+            "&:hover": {
+              backgroundColor: "#e0e0e0",
+              opacity: [0.9, 0.8, 0.7],
+            },
+            p: 1,
+            m: 1,
+          }}
+        >
+          <span style={{ marginLeft: "4em" }}> My Team</span>
+        </Box>
+      </Link>
+      <Link
+        to={`${url}/ticket`}
+        style={{ textDecoration: "none", color: "#1769aa" }}
+      >
+        <Box
+          sx={{
+            backgroundColor: "",
+            "&:hover": {
+              backgroundColor: "#e0e0e0",
+              opacity: [0.9, 0.8, 0.7],
+            },
+            p: 1,
+            m: 1,
+          }}
+        >
+          <span style={{ marginLeft: "4em" }}>Create Ticket </span>
+        </Box>
+      </Link>
+      <Link
+        to={`${url}/myTicket`}
+        style={{ textDecoration: "none", color: "#1769aa" }}
+      >
+        <Box
+          sx={{
+            backgroundColor: "",
+            "&:hover": {
+              backgroundColor: "#e0e0e0",
+              opacity: [0.9, 0.8, 0.7],
+            },
+            p: 1,
+            m: 1,
+          }}
+        >
+          <span style={{ marginLeft: "4em" }}>My Ticket </span>
+        </Box>
+      </Link>
+      {currentUser?.role == "manager" && (
+        <Link
+          to={`${url}/assign`}
+          style={{ textDecoration: "none", color: "#1769aa" }}
+        >
+          <Box
+            sx={{
+              backgroundColor: "",
+              "&:hover": {
+                backgroundColor: "#e0e0e0",
+                opacity: [0.9, 0.8, 0.7],
+              },
+              p: 1,
+              m: 1,
+            }}
+          >
+            <span style={{ marginLeft: "4em" }}> Assign Ticket </span>
+          </Box>
+        </Link>
+      )}
     </div>
   );
 
@@ -100,93 +134,100 @@ function ResponsiveDrawer(props) {
     window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          ml: { sm: `${drawerWidth}px` },
-          pb:1,
-        }}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: "none" } }}
+    <>
+      {currentUser?.email == undefined ? (
+        <CircularProgress />
+      ) : (
+        <Box sx={{ display: "flex" }}>
+          <CssBaseline />
+          <AppBar
+            position="fixed"
+            sx={{
+              width: { sm: `calc(100% - ${drawerWidth}px)` },
+              ml: { sm: `${drawerWidth}px` },
+              pb: 1,
+            }}
           >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            Responsive drawer
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Box
-        component="nav"
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-        aria-label="mailbox folders"
-      >
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-        >
-          {drawer}
-        </Drawer>
-        <Drawer
-          variant="permanent"
-          sx={{
-            display: { xs: "none", sm: "block" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-          open
-        >
-          {drawer}
-        </Drawer>
-      </Box>
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          p: 3,
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-        }}
-      >
-        <Toolbar />
-       <Switch>
-           <Route  exact path={path}>
-                <Team/>
-           </Route>
-           <Route  path={`${path}/ticket`}>
-              <Ticket />
-           </Route>
-           <ManagerRoute  path={`${path}/assign`}>
-             <AssignTicket />
-           </ManagerRoute>
-
-
-       </Switch>
-      </Box>
-    </Box>
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                edge="start"
+                onClick={handleDrawerToggle}
+                sx={{ mr: 2, display: { sm: "none" } }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" noWrap component="div">
+                Responsive drawer
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Box
+            component="nav"
+            sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+            aria-label="mailbox folders"
+          >
+            {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+            <Drawer
+              container={container}
+              variant="temporary"
+              open={mobileOpen}
+              onClose={handleDrawerToggle}
+              ModalProps={{
+                keepMounted: true, // Better open performance on mobile.
+              }}
+              sx={{
+                display: { xs: "block", sm: "none" },
+                "& .MuiDrawer-paper": {
+                  boxSizing: "border-box",
+                  width: drawerWidth,
+                },
+              }}
+            >
+              {drawer}
+            </Drawer>
+            <Drawer
+              variant="permanent"
+              sx={{
+                display: { xs: "none", sm: "block" },
+                "& .MuiDrawer-paper": {
+                  boxSizing: "border-box",
+                  width: drawerWidth,
+                },
+              }}
+              open
+            >
+              {drawer}
+            </Drawer>
+          </Box>
+          <Box
+            component="main"
+            sx={{
+              flexGrow: 1,
+              p: 3,
+              width: { sm: `calc(100% - ${drawerWidth}px)` },
+            }}
+          >
+            <Toolbar />
+            <Switch>
+              <Route exact path={path}>
+                <Team />
+              </Route>
+              <Route path={`${path}/ticket`}>
+                <Ticket />
+              </Route>
+              <Route path={`${path}/myTicket`}>
+                <MyTicket />
+              </Route>
+              <ManagerRoute path={`${path}/assign`}>
+                <AssignTicket />
+              </ManagerRoute>
+            </Switch>
+          </Box>
+        </Box>
+      )}
+    </>
   );
 }
 
