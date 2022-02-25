91ab1c0a8b77abb31fc49cb31e5574ee6c2d2da1
nub69 <shabik.ashab@gmail.com>
2022-02-25 21:09:36 +0600

    comments are shown in the ui

--- src/Components/Dashboard/AddComment.jsx
+++ src/Components/Dashboard/AddComment.jsx
@@ -1,64 +1,108 @@
-import { Button, TextField } from '@mui/material';
-import { Box } from '@mui/system';
-import React, { useState } from 'react'
+import { Button, Skeleton, TextField, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useEffect, useState } from "react";
 
-const AddComment = ({user,id}) => {
-    const [success,setSuccess] = useState(false);
-    const [loading,setLoading] = useState(false);
+const AddComment = ({ user, id }) => {
+  const [success, setSuccess] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [comments, setComments] = useState([]);
 
-    const initialInfo = { name: user.displayName, email: user.email,id:id};
-    const [newData, setNewData] = useState(initialInfo);
+  const initialInfo = { name: user.displayName, email: user.email, id: id };
+  const [newData, setNewData] = useState(initialInfo);
 
-    const handleOnBlur = (e) => {
-        const field = e.target.name;
-        const value = e.target.value;
-        const newFormData = { ...newData };
-        newFormData[field] = value;
-        setNewData(newFormData);
-      };
+  useEffect(() => {
+    const url = `http://localhost:5000/comments`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setComments(data));
+  }, [loading]);
 
-      const handleSubmit = () =>{
-        
-        setLoading(true);
-       
-       
-        
-        fetch('http://localhost:5000/comments', {
-              method: 'POST',
-              headers: {
-                  'content-type': 'application/json'
-              },
-             body: JSON.stringify(newData),
-          })
-              .then(res => res.json())
-              .then(data => {
-                  if (data.insertedId) {
-                      setSuccess(true);
-                      setLoading(false);
-                  }
-              })
-        
-      }
+  const commentOnTicket = comments.filter((c) => c.id == id);
+  console.log(commentOnTicket);
+  const handleOnBlur = (e) => {
+    const field = e.target.name;
+    const value = e.target.value;
+    const newFormData = { ...newData };
+    newFormData[field] = value;
+    setNewData(newFormData);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    if(!newData.comment){
+        alert('Please write something!');
+        setLoading(false);
+        return;
+    }
+
+    fetch("http://localhost:5000/comments", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          e.target.reset();
+          setNewData("")
+          setSuccess(true);
+          setLoading(false);
+          
+        }
+      });
+  };
 
   return (
     <>
-        <Box>
+      <form onSubmit={handleSubmit}>
         <TextField
-              sx={{
-                  mt:2
-              }} 
-              label="Add a comment..."
-              name="comment"
-              multiline
-              variant="filled"
-              onBlur={handleOnBlur}
-            />
-            <Button onClick={handleSubmit} sx={{mt:3,ml:2}} type="submit" variant="outlined">Add Comment</Button>
+          sx={{
+            mt: 2,
+          }}
+          label="Add a comment..."
+          name="comment"
+          multiline
+          variant="filled"
+          onBlur={handleOnBlur}
+        />
+        <Button
+          type="submit"
+          sx={{ mt: 3, ml: 2 }}
+          type="submit"
+          variant="outlined"
+        >
+          Add Comment
+        </Button>
+      </form>
 
-              
-        </Box>
+      <Box sx={{ mt: 2, mb: 2 }}>
+        <hr />
+      </Box>
+
+      <Box>
+          {
+              commentOnTicket.map((comment) => (
+                  <Box sx={{backgroundColor: "#e0e0e0",m:1,p:2,width:'40%'}}>
+                    <Typography sx={{fontSize:'.7em',}}>
+                        {comment.name}
+                    </Typography>
+                    <Typography >
+                        {comment.comment}
+                    </Typography>
+                    
+                  </Box>
+              ))
+          }
+          {
+                        loading &&
+                        <Skeleton sx={{m:1}} variant="rectangular" width="40%"  height={80} />
+                    }
+      </Box>
     </>
-  )
-}
+  );
+};
 
-export default AddComment
+export default AddComment;
